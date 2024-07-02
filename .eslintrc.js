5112baaa53ba915f8de11fdb2162a8f30c617453
shahzain46 <shahzain6016@gmail.com>
2024-07-02 06:39:20 -0500

    @phase 1

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "browser": true,
+    "jest": true,
+  },
+  "extends": "airbnb",
+  "rules": {
+    "import/no-extraneous-dependencies": "off",
+    "import/no-cycle": "off",
+    "react/jsx-one-expression-per-line": "off",
+    "react/jsx-filename-extension": "off",
+    "react/forbid-prop-types": "off",
+  }
+};
